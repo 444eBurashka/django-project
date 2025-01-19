a6f5d1e70f77fafceea8ec822fc2ce744eeae346
444eBurashka <matveykhorev@yandex.ru>
2025-01-20 01:33:27 +0500

    django static 2

--- profsort/profsort/urls.py
+++ profsort/profsort/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vactables.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
