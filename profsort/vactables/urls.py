0cf7f3fc6e9953b2621f6ee1a7c5ec0695fa606b
444eBurashka <matveykhorev@yandex.ru>
2025-01-20 09:57:21 +0500

    valid urls api

--- profsort/vactables/urls.py
+++ profsort/vactables/urls.py
@@ -3,15 +3,15 @@ from .api import *
 
 
 router = DefaultRouter()
-router.register('api/general', GeneralViewSet, 'General')
-router.register('api/quantity', QuantityViewSet, 'Quantity')
-router.register('api/level', LevelViewSet, 'Level')
-router.register('api/fraction', FractionViewSet, 'Fraction')
-router.register('api/top', TopViewSet, 'Top')
-router.register('api/generalpython', GeneralPythonViewSet, 'GeneralPython')
-router.register('api/quantitypython', QuantityPythonViewSet, 'QuantityPython')
-router.register('api/levelpython', LevelPythonViewSet, 'LevelPython')
-router.register('api/fractionpython', FractionPythonViewSet, 'FractionPython')
-router.register('api/toppython', TopPythonViewSet, 'TopPython')
+router.register('general', GeneralViewSet, 'General')
+router.register('quantity', QuantityViewSet, 'Quantity')
+router.register('level', LevelViewSet, 'Level')
+router.register('fraction', FractionViewSet, 'Fraction')
+router.register('top', TopViewSet, 'Top')
+router.register('generalpython', GeneralPythonViewSet, 'GeneralPython')
+router.register('quantitypython', QuantityPythonViewSet, 'QuantityPython')
+router.register('levelpython', LevelPythonViewSet, 'LevelPython')
+router.register('fractionpython', FractionPythonViewSet, 'FractionPython')
+router.register('toppython', TopPythonViewSet, 'TopPython')
 
 urlpatterns = router.urls
